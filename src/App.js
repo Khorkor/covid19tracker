3883d7a01a1a98b680bb346bdfc88f0a8936fd25
Zakir Baytar <zakir.baytar@eyedius.com>
2020-03-27 15:44:31 +0300

    Added: Confirmed, deaths and recovered lists to homepage.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-import CountryList from "./components/ConfirmedCountry/CountryList";
+import CovidReportProvider from "./components/CovidReportProvider/CovidReportProvider";
+
+import TotalConfirmedList from "./components/TotalConfirmed/TotalConfirmedList";
+import TotalDeathsList from "./components/TotalDeaths/TotalDeathsList";
+import TotalRecoveredList from "./components/TotalRecovered/TotalRecoveredList";
 
 //apollo client setup
 const client = new ApolloClient({
@@ -13,11 +17,27 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="table-wrapper-scroll-y my-custom-scrollbar">
-          <h1>Total Confirmed</h1>
-
-          <CountryList />
-        </div>
+        <CovidReportProvider>
+          <div className="full-container">
+            <div className="row">
+              <div className="col-2 col-sm-2 col-md-2 col-lg-2">
+                <TotalConfirmedList />
+              </div>
+              <div
+                className="col-6 col-sm-6 col-md-6 col-lg-6"
+                style={{ alignSelf: "center", textAlign: "center" }}
+              >
+                <h1 style={{ flex: 1 }}>Harita ya da filtrelenebilir tablo </h1>
+              </div>
+              <div className="col-2 col-sm-2 col-md-2 col-lg-2">
+                <TotalDeathsList />
+              </div>
+              <div className="col-2 col-sm-2 col-md-2 col-lg-2">
+                <TotalRecoveredList />
+              </div>
+            </div>
+          </div>
+        </CovidReportProvider>
       </ApolloProvider>
     );
   }
